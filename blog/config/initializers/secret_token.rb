692cb0385a98f9b45af47234deeca2f9eea22f7a
yashpreet <yashpreet@hoppr.com>
2012-12-08 12:59:05 +0530

    Manan-added basic project skeleton

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '15a5351f702fa45dbdb976ff8d7469c4ad9372a5bf4c843b61ff78c5ee28568308c7c0dc54117838c31d1bf7536cc87f16f5e0bf43d1f53a0ec5ef9d73d985d0'
