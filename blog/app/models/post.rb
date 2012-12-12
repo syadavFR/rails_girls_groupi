9f16d8c621ecc3dbd62af88ead6f88dbd2efdbec
yashpreet <yashpreet@hoppr.com>
2012-12-12 15:45:35 +0530

    added validations to post and user

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ActiveRecord::Base
   attr_accessible :created_date, :description, :modified_date, :title
+  
+  validates :description, :presence => true
+  validates :title, :presence => true
+ 
   has_many :comments
- belongs_to :user 
+  belongs_to :user 
 end
--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name
+
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+
  	 has_many :comments
 	 has_many :posts
 end
